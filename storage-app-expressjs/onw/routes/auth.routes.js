22ef732dc9345332ad7e9dfccc6eade0f4b3646e
theniteshdev <0911nitesh@gmail.com>
2026-06-09 19:56:32 +0530

    implement email validation in register route

--- storage-app-expressjs/onw/routes/auth.routes.js
+++ storage-app-expressjs/onw/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dirTreeDB from '../dirTreeDB.json' with {type: "json"};
 import userDB from '../userDB.json' with {type: "json"};
+import emailDB from "../allEmailsBD.json" with {type: "json"};
 import { writeFile } from 'node:fs/promises';
 import { randomUUID } from "node:crypto";
 
 const dirDBArray = dirTreeDB;
 const userDBArray = userDB;
+const emailDBArray = emailDB;
 
 const routes = express.Router();
 
@@ -18,7 +20,14 @@ routes.post("/signup", async (req, res, next) => {
             "message": "all fields are required!"
         })
     }
-
+    for (const email of emailDBArray) {
+        if (email === req.body.email) {
+            return res.status(400).json({
+                "message": "email already exists"
+            });
+            break;
+        }
+    }
     const userId = randomUUID();
     const rootDirId = randomUUID();
 
@@ -35,11 +44,13 @@ routes.post("/signup", async (req, res, next) => {
         "email": req.body.email,
         "passowrd": req.body.password,
         "rootDirId": rootDirId
-    })
+    });
+    emailDBArray.push(req.body.email);
 
     try {
         writeFile(`${process.cwd()}/dirTreeDB.json`, JSON.stringify(dirDBArray));
         writeFile(`${process.cwd()}/userDB.json`, JSON.stringify(userDBArray));
+        writeFile(`${process.cwd()}/allEmailsBD.json`, JSON.stringify(emailDBArray));
     } catch (error) {
         return res.status(500).json({
             "message": "registration failed!"
