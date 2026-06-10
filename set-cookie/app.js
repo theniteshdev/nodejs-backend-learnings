8280c4266a50d36933becfa9700ce286ed5414c6
theniteshdev <0911nitesh@gmail.com>
2026-06-10 18:26:07 +0530

    fix

--- set-cookie/app.js
+++ set-cookie/app.js
@@ -1,12 +1,18 @@
 import http from 'node:http'
-const PORT = 80;
+const PORT = 4001;
 const HOSTNANE = '0.0.0.0';
 const server = http.createServer((req, res) => {
-    res.end("Ok");
+    // res.setHeader("Set-Cookie", "username=theniteshdev")
+    // res.setHeader("Set-Cookie", "id=hu5uf")
+    // res.setHeader("Set-Cookie", "address=lakshmana-nagar;httpOnly")
+    // res.setHeader("Set-Cookie", `currentTime=${new Date().toLocaleString()}`)
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.setHeader("Access-Control-Allow-Credentials", "true")
     console.log(req.headers.cookie);
     if (req.url == "/login") {
-        res.setHeader("Set-Cookie", "sessionId=abc123; Max-Age:60; HttpOnly");
+        res.setHeader("Set-Cookie", "sessionId=abc123; Max-Age:60; HttpOnly;Secure;SameSite=None;");
     }
+    res.end("Ok");
 });
 
 server.listen(PORT, HOSTNANE, (err) => {
