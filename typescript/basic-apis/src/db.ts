856d9cfb357baac133dab92e3874a9d79b45a66a
theniteshdev <0911nitesh@gmail.com>
2026-06-24 01:12:01 +0530

    ts

---
+++ typescript/basic-apis/src/db.ts
@@ -0,0 +1,16 @@
+import { MongoClient, Db } from "mongodb";
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+let db: Db;
+
+export async function connectDB() {
+  await client.connect();
+  db = client.db("blog_db");
+  console.log("Connected to MongoDB");
+}
+
+export function getDB() {
+  return db;
+}
